1be634e6443dbe0e95dcd79ab2bc33fd56bca8fe
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-06-21 22:40:00 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Error from "./ui/Error";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -10,6 +11,7 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
