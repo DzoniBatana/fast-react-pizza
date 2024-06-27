b6b2dfb69e7a3e8c58172d7c7cc42d09e96680b7
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-06-27 23:10:46 +0200

    Update

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Nikola</div>;
+  return <div className="hidden text-sm font-semibold md:block">Nikola</div>;
 }
 
 export default Username;
