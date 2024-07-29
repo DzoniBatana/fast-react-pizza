c09d8f9e17241716388a29e51954c4f322c7a3e0
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-07-29 23:02:48 +0200

    Update

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,4 +1,6 @@
+import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
+import { getCurrentQuantityById } from './cartSlice';
 import DeleteItem from './DeleteItem';
 import UpdateItemQuantity from './UpdateItemQuantity';
 
@@ -6,6 +8,9 @@ import UpdateItemQuantity from './UpdateItemQuantity';
 function CartItem({ item }) {
   // eslint-disable-next-line
   const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
+  //console.log(currentQuantity);
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -15,7 +20,10 @@ function CartItem({ item }) {
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
 
-        <UpdateItemQuantity pizzaId={pizzaId} />
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -3,16 +3,17 @@ import Button from '../../ui/Button';
 import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
 // eslint-disable-next-line
-function UpdateItemQuantity({ pizzaId }) {
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
   const dispatch = useDispatch();
   return (
-    <div className="flex-gap-2 items-center md:gap-3">
+    <div className="flex items-center gap-2 md:gap-3">
       <Button
         type="round"
         onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
       >
         -
       </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
       <Button
         type="round"
         onClick={() => dispatch(increaseItemQuantity(pizzaId))}
