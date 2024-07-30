4b677f628a8f500a424b93184cc9a49ad307b296
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-07-30 23:38:28 +0200

    Update

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,13 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
 import { useState } from 'react';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 // eslint-disable-next-line
@@ -22,6 +23,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   // eslint-disable-next-line
   const cart = useSelector(getCart);
@@ -34,6 +36,7 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
 
       {/* <Form method='POST' action={'/order/new'} */}
       <Form method="POST">
